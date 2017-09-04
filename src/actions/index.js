2410272d63fa14e534041f3eadd7f97fd8f83980
Nick Foden <nickfoden@gmail.com>
2017-09-05 00:32:50 +0100

    Adding dispatch action

--- src/actions/index.js
+++ src/actions/index.js
@@ -10,3 +10,20 @@ export const deleteTodo = (id) => ({
   type: DELETE_TODO,
   id
 })
+
+export const saveTodo =  (text) => {
+  return dispatch => {
+    fetch('http://localhost:8080/api/todos', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        text
+      })
+    })
+    .then(response => response.json())
+    .then(json => console.log(json))
+    .catch(err => console.log(err))
+  }
+}
