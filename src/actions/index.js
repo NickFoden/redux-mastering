a4638e3036c2cfb37c7cd0fbf6bdae04314e8949
Nick Foden <nickfoden@gmail.com>
2017-09-06 20:28:44 -0400

    Changing actions to be a save async request to server

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,12 +1,22 @@
-const ADD_TODO = 'ADD_TODO'
-export const addTodo = (text) => ({
-  type: ADD_TODO,
-  text,
-  id: Math.random()
-})
-
 const DELETE_TODO = 'DELETE_TODO'
 export const deleteTodo = (id) => ({
   type: DELETE_TODO,
   id
 })
+
+export const saveTodo = (text) => {
+  return dispatch => {
+    fetch('http://localhost:8080/api/todos', {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify({
+        text
+      })
+    })
+    .then(response => response.json())
+    .then(json => console.log(json))
+    .catch(err => console.log(err))
+  }
+}
